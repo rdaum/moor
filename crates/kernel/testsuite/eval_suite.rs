5ccc2d19de4820bfb5b81f969b1259b048302893
Zoltan Nagy <abesto0@gmail.com>
2024-05-28 22:37:38 +0100

    refactor(eval_suite): syntax sugar for `eval()` + `assert_eq`

--- crates/kernel/testsuite/eval_suite.rs
+++ crates/kernel/testsuite/eval_suite.rs
@@ -13,97 +13,77 @@
 //
 
 mod common;
-use common::{create_db, eval, WIZARD};
+use common::{create_db, AssertEval, WIZARD};
 use moor_values::{
     var::{
-        v_empty_list, v_empty_str,
+        v_empty_list, v_empty_str, v_none,
         Error::{E_ARGS, E_PERM, E_TYPE},
     },
     NOTHING,
 };
-use pretty_assertions::assert_eq;
 
 #[test]
 fn test_that_eval_cannot_be_called_by_non_programmers() {
     let db = create_db();
-    eval(db.clone(), WIZARD, "player.programmer = 0;");
-    assert_eq!(eval(db.clone(), WIZARD, r#"return 5;"#), E_PERM.into());
+    db.assert_eval(WIZARD, "player.programmer = 0;", v_none());
+    db.assert_eval(WIZARD, "return 5;", E_PERM);
 }
 
 #[test]
 fn test_bf_eval_cannot_be_called_by_non_programmers() {
     let db = create_db();
-    assert_eq!(
-        eval(
-            db.clone(),
-            WIZARD,
-            r#"player.programmer = 0; return eval("return 5;");"#
-        )
-        .unwrap_err()
-        .code,
-        E_PERM
+    db.assert_eval_exception(
+        WIZARD,
+        r#"player.programmer = 0; return eval("return 5;");"#,
+        E_PERM,
     );
 }
 
 #[test]
 fn test_that_bf_eval_requires_at_least_one_argument() {
     let db = create_db();
-    assert_eq!(eval(db, WIZARD, "return eval();").unwrap_err().code, E_ARGS);
+    db.assert_eval_exception(WIZARD, "return eval();", E_ARGS);
 }
 
 #[test]
 fn test_that_eval_requires_string_arguments() {
     let db = create_db();
-    assert_eq!(
-        eval(db.clone(), WIZARD, "return eval(1);")
-            .unwrap_err()
-            .code,
-        E_TYPE
-    );
+    db.assert_eval_exception(WIZARD, "return eval(1);", E_TYPE);
     // TODO uncomment when `eval()` gets support for multiple arguments
-    // assert_eq!(eval(db, WIZARD, "return eval(1, 2);"), E_ARGS.into());
-    assert_eq!(
-        eval(db, WIZARD, "return eval({});").unwrap_err().code,
-        E_TYPE
-    );
+    // db.assert_eval_exception(WIZARD, "return eval(1, 2);", E_TYPE);
+    db.assert_eval_exception(WIZARD, "return eval({});", E_TYPE);
 }
 
 #[test]
 #[ignore = "We don't currently support multiple args to eval()"]
 fn test_that_eval_evaluates_multiple_strings() {
     let db = create_db();
-    assert_eq!(
-        eval(
-            db,
-            WIZARD,
-            r#"return eval("x = 0;", "for i in [1..5]", "x = x + i;", "endfor", "return x;");"#
-        ),
-        [1, 15].into()
+    db.assert_eval(
+        WIZARD,
+        r#"return eval("x = 0;", "for i in [1..5]", "x = x + i;", "endfor", "return x;");"#,
+        [1, 15],
     );
 }
 
 #[test]
 fn test_that_eval_evaluates_a_single_string() {
     let db = create_db();
-    assert_eq!(
-        eval(db, WIZARD, r#"return eval("return 5;");"#),
-        [1, 5].into()
-    );
+    db.assert_eval(WIZARD, r#"return eval("return 5;");"#, [1, 5]);
 }
 
 #[test]
 fn test_eval_builtin_variables() {
     // As seen on https://stunt.io/ProgrammersManual.html#Language
     let db = create_db();
-    assert_eq!(eval(db.clone(), WIZARD, "return player;"), WIZARD.into());
-    assert_eq!(eval(db.clone(), WIZARD, "return this;"), NOTHING.into());
-    assert_eq!(eval(db.clone(), WIZARD, "return caller;"), WIZARD.into());
-    assert_eq!(eval(db.clone(), WIZARD, "return args;"), v_empty_list());
-    assert_eq!(eval(db.clone(), WIZARD, "return argstr;"), v_empty_str());
-    assert_eq!(eval(db.clone(), WIZARD, "return verb;"), v_empty_str());
-    assert_eq!(eval(db.clone(), WIZARD, "return dobjstr;"), v_empty_str());
-    assert_eq!(eval(db.clone(), WIZARD, "return dobj;"), NOTHING.into());
-    assert_eq!(eval(db.clone(), WIZARD, "return prepstr;"), v_empty_str());
-    assert_eq!(eval(db.clone(), WIZARD, "return iobjstr;"), v_empty_str());
-    assert_eq!(eval(db.clone(), WIZARD, "return iobj;"), NOTHING.into());
+    db.assert_eval(WIZARD, "return player;", WIZARD);
+    db.assert_eval(WIZARD, "return this;", NOTHING);
+    db.assert_eval(WIZARD, "return caller;", WIZARD);
+    db.assert_eval(WIZARD, "return args;", v_empty_list());
+    db.assert_eval(WIZARD, "return argstr;", v_empty_str());
+    db.assert_eval(WIZARD, "return verb;", v_empty_str());
+    db.assert_eval(WIZARD, "return dobjstr;", v_empty_str());
+    db.assert_eval(WIZARD, "return dobj;", NOTHING);
+    db.assert_eval(WIZARD, "return prepstr;", v_empty_str());
+    db.assert_eval(WIZARD, "return iobjstr;", v_empty_str());
+    db.assert_eval(WIZARD, "return iobj;", NOTHING);
 }
