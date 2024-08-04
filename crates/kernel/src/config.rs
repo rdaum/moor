635d8418a2982f8af4033ec12ed67733606e4903
Ryan Daum <ryan.daum@gmail.com>
2024-08-04 09:50:52 -0400

    Add textdump encoding mode to Config

--- crates/kernel/src/config.rs
+++ crates/kernel/src/config.rs
@@ -15,9 +15,11 @@
 //! Config is created by the host daemon, and passed through the scheduler, whereupon it is
 //! available to all components. Used to hold things typically configured by CLI flags, etc.
 
+use crate::textdump::EncodingMode;
 use std::path::PathBuf;
 
 #[derive(Debug, Default)]
 pub struct Config {
     pub textdump_output: Option<PathBuf>,
+    pub textdump_encoding: EncodingMode,
 }
