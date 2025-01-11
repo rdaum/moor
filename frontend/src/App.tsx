a8e24dacfcda00d842601506d9a423d0c4ad0931
Ryan Daum <ryan.daum@gmail.com>
2025-01-11 14:05:51 -0500

    Cleanups in the web UI & licensure fixes
    Properly scrolls on input, nicer font and spacing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,65 @@
-import { useEffect, useState } from "react";
+// Copyright (C) 2025 Ryan Daum <ryan.daum@gmail.com> This program is free
+// software: you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation, version
+// 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
+const Messages = ({ messages }) => {
+	const messagesEndRef = useRef(null);
+	const scrollToBottom = () => {
+		messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+	};
+	useEffect(scrollToBottom, [messages]);
+
+	return (
+		<div className="messages">
+			{messages.map(message => (
+				<div key={message}>{message}</div>
+			))}
+			<div ref={messagesEndRef} />
+		</div>
+	);
+};
+
+const InputField = ({ setMessage, sendMessage }) => {
+	const inputRef = useRef(null);
+	const onEntry = () => {
+		inputRef.current.focus();
+		sendMessage();
+		inputRef.current.value = "";
+	};
+	return (
+		<div className="inputArea">
+			<input
+				onChange={(e) => setMessage(e.target.value)}
+				type="text"
+				ref={inputRef}
+				placeholder="message"
+				className="inputField"
+				onKeyDown={(e) => e.key === "Enter" && onEntry()}
+			/>
+			<button onClick={onEntry} className="inputSubmitButton" type="button">
+				Send
+			</button>
+		</div>
+	);
+}
 function App() {
 	const [messages, setMessages] = useState([]);
 	const [socket, setSocket] = useState(null);
 	const [message, setMessage] = useState("");
+
+
 
 	function sendMessage() {
 		socket.send(
@@ -33,26 +88,11 @@ function App() {
 
 	return (
 		<div>
-			<div>
-				{messages.map((m, index) => (
-					<div key={index}>
-						{m}
-					</div>
-				))}
-			</div>
-			<div>
-				<input
-					onChange={(e) => setMessage(e.target.value)}
-					type="text"
-					placeholder="message"
-					onKeyDown={(e) => e.key === "Enter" && sendMessage()}
-				/>
-				<button onClick={sendMessage}>
-					Send
-				</button>
-			</div>
+			<Messages messages={messages} />
+			<InputField setMessage={setMessage} sendMessage={sendMessage} />
 		</div>
 	);
 }
+
 
 export default App;
