44c12941fdb2f6b66438d357984680aa5828fa0f
Ryan Daum <ryan.daum@gmail.com>
2025-02-15 09:52:08 -0500

    Avoid constant rebuilds in debug builds

--- crates/common/build.rs
+++ crates/common/build.rs
@@ -15,7 +15,7 @@ use shadow_rs::{BuildPattern, ShadowBuilder};
 
 fn main() {
     ShadowBuilder::builder()
-        .build_pattern(BuildPattern::RealTime)
+        .build_pattern(BuildPattern::Lazy)
         .build()
         .unwrap();
 }
