c6bda64f460279ded87716d3d2f86941bebda4e2
Ryan Daum <ryan.daum@gmail.com>
2025-04-11 00:14:57 -0400

    Just switch to plain old rust structs for Defs and VerbDef/PropDef
    No performance advantage these days on the DB side, and just makes
    things complicated.

--- crates/common/src/model/verbdef.rs
+++ crates/common/src/model/verbdef.rs
@@ -11,46 +11,30 @@
 // this program. If not, see <https://www.gnu.org/licenses/>.
 //
 
-use crate::DATA_LAYOUT_VERSION;
 use crate::model::defset::{Defs, HasUuid, Named};
 use crate::model::r#match::VerbArgsSpec;
 use crate::model::verbs::{BinaryType, VerbFlag};
 use crate::util::BitEnum;
 use crate::util::verbname_cmp;
-use binary_layout::{Field, binary_layout};
-use byteview::ByteView;
-use moor_var::AsByteBuffer;
+use bincode::{Decode, Encode};
+use moor_var::BincodeAsByteBufferExt;
 use moor_var::Obj;
 use moor_var::Symbol;
-use moor_var::encode::{DecodingError, EncodingError};
 use uuid::Uuid;
 
-#[derive(Debug, Eq, PartialEq)]
-pub struct VerbDef(ByteView);
-
-impl Clone for VerbDef {
-    fn clone(&self) -> Self {
-        Self(self.0.to_detached().clone())
-    }
+#[derive(Clone, Debug, Eq, PartialEq, Encode, Decode)]
+pub struct VerbDef {
+    #[bincode(with_serde)]
+    uuid: Uuid,
+    location: Obj,
+    owner: Obj,
+    flags: BitEnum<VerbFlag>,
+    binary_type: BinaryType,
+    args: VerbArgsSpec,
+    names: Vec<Symbol>,
 }
 
-binary_layout!(verbdef, LittleEndian, {
-    data_version: u8,
-    uuid: [u8; 16],
-    location: Obj as u64,
-    owner: Obj as u64,
-    flags: BitEnum::<VerbFlag> as u16,
-    binary_type: BinaryType as u8,
-    args: VerbArgsSpec as u32,
-    num_names: u8,
-    names: [u8],
-});
-
 impl VerbDef {
-    fn from_bytes(bytes: ByteView) -> Self {
-        Self(bytes)
-    }
-
     #[must_use]
     pub fn new(
         uuid: Uuid,
@@ -61,92 +45,38 @@ impl VerbDef {
         binary_type: BinaryType,
         args: VerbArgsSpec,
     ) -> Self {
-        let header_size = verbdef::names::OFFSET;
-        let num_names = names.len();
-        let names_region_size = names.iter().map(|n| n.len()).sum::<usize>() + num_names;
-        let total_size = header_size + names_region_size;
-
-        let mut buffer = vec![0; total_size];
-
-        let mut verbdef_layout = verbdef::View::new(&mut buffer);
-        verbdef_layout.data_version_mut().write(DATA_LAYOUT_VERSION);
-        verbdef_layout.uuid_mut().copy_from_slice(uuid.as_bytes());
-        verbdef_layout
-            .location_mut()
-            .try_write(location)
-            .expect("Failed to encode location");
-        verbdef_layout
-            .owner_mut()
-            .try_write(owner)
-            .expect("Failed to encode owner");
-        verbdef_layout
-            .flags_mut()
-            .try_write(flags)
-            .expect("Failed to encode flags");
-        verbdef_layout
-            .binary_type_mut()
-            .try_write(binary_type)
-            .expect("Failed to encode binary_type");
-        verbdef_layout
-            .args_mut()
-            .try_write(args)
-            .expect("Failed to encode args");
-        verbdef_layout.num_names_mut().write(names.len() as u8);
-
-        // Now write the names, into the names region.
-        let names_buf = verbdef_layout.names_mut();
-        let mut position = 0;
-        for name in names {
-            names_buf[position] = name.len() as u8;
-            position += 1;
-            names_buf[position..position + name.len()].copy_from_slice(name.as_bytes());
-            position += name.len();
+        Self {
+            uuid,
+            location,
+            owner,
+            flags,
+            binary_type,
+            args,
+            names: names.iter().map(|s| (*s).into()).collect(),
         }
-
-        Self(ByteView::from(buffer))
-    }
-
-    fn get_header_view(&self) -> verbdef::View<&[u8]> {
-        let view = verbdef::View::new(self.0.as_ref());
-        assert_eq!(
-            view.data_version().read(),
-            DATA_LAYOUT_VERSION,
-            "Unsupported data layout version: {}",
-            view.data_version().read()
-        );
-        view
     }
 
     #[must_use]
     pub fn location(&self) -> Obj {
-        let view = self.get_header_view();
-        view.location()
-            .try_read()
-            .expect("Failed to decode location")
+        self.location.clone()
     }
+
     #[must_use]
     pub fn owner(&self) -> Obj {
-        let view = self.get_header_view();
-        view.owner().try_read().expect("Failed to decode owner")
+        self.owner.clone()
     }
     #[must_use]
     pub fn flags(&self) -> BitEnum<VerbFlag> {
-        let view = self.get_header_view();
-        view.flags().try_read().expect("Failed to decode flags")
+        self.flags
     }
+
     #[must_use]
     pub fn binary_type(&self) -> BinaryType {
-        let view = self.get_header_view();
-        view.binary_type()
-            .try_read()
-            .expect("Failed to decode binary_type")
+        self.binary_type
     }
     #[must_use]
     pub fn args(&self) -> VerbArgsSpec {
-        let view = self.get_header_view();
-        view.args()
-            .try_read()
-            .expect("Failed to decode verb args spec")
+        self.args
     }
 
     pub fn matches_spec(
@@ -176,67 +106,24 @@ impl Named for VerbDef {
 
     #[must_use]
     fn names(&self) -> Vec<&str> {
-        let view = self.get_header_view();
-        let num_names = view.num_names().read() as usize;
-        let offset = verbdef::names::OFFSET;
-        let slice = &self.0.as_ref()[offset..];
-        let mut position = 0;
-        let mut names = Vec::with_capacity(num_names);
-        for _ in 0..num_names {
-            let length = slice[position];
-            position += 1;
-            let name_slice = &slice[position..position + length as usize];
-            position += length as usize;
-            names.push(std::str::from_utf8(name_slice).unwrap());
-        }
-        names
+        self.names.iter().map(|s| s.as_str()).collect()
     }
 }
 
 impl HasUuid for VerbDef {
     fn uuid(&self) -> Uuid {
-        let view = self.get_header_view();
-        Uuid::from_bytes(*view.uuid())
+        self.uuid
     }
 }
 
-impl AsByteBuffer for VerbDef {
-    fn size_bytes(&self) -> usize {
-        self.0.len()
-    }
-
-    fn with_byte_buffer<R, F: FnMut(&[u8]) -> R>(&self, mut f: F) -> Result<R, EncodingError> {
-        Ok(f(self.0.as_ref()))
-    }
-
-    fn make_copy_as_vec(&self) -> Result<Vec<u8>, EncodingError> {
-        Ok(self.0.as_ref().to_vec())
-    }
-
-    fn from_bytes(bytes: ByteView) -> Result<Self, DecodingError> {
-        let header = verbdef::View::new(bytes.as_ref());
-        assert_eq!(header.data_version().read(), DATA_LAYOUT_VERSION);
-        Ok(Self::from_bytes(bytes))
-    }
-
-    fn as_bytes(&self) -> Result<ByteView, EncodingError> {
-        Ok(self.0.to_detached())
-    }
-}
+impl BincodeAsByteBufferExt for VerbDef {}
 
 pub type VerbDefs = Defs<VerbDef>;
 
 #[cfg(test)]
 mod tests {
-    use crate::model::ValSet;
-    use crate::model::defset::{HasUuid, Named};
-    use crate::model::r#match::VerbArgsSpec;
-    use crate::model::verbdef::{VerbDef, VerbDefs};
     use crate::model::verbs::VerbFlag;
     use crate::util::BitEnum;
-    use byteview::ByteView;
-    use moor_var::AsByteBuffer;
-    use moor_var::Obj;
 
     #[test]
     fn test_bitflags() {
@@ -254,107 +141,5 @@ mod tests {
             VerbFlag::rwxd(),
             BitEnum::new() | VerbFlag::Read | VerbFlag::Write | VerbFlag::Exec | VerbFlag::Debug
         );
-    }
-    #[test]
-    fn test_reconstitute() {
-        let vd = VerbDef::new(
-            uuid::Uuid::new_v4(),
-            Obj::mk_id(1),
-            Obj::mk_id(1),
-            &["foo", "bar"],
-            VerbFlag::rwxd(),
-            crate::model::verbs::BinaryType::LambdaMoo18X,
-            VerbArgsSpec::this_none_this(),
-        );
-
-        let bytes = vd.with_byte_buffer(<[u8]>::to_vec).unwrap();
-        let vd2 = VerbDef::from_bytes(ByteView::from(bytes));
-
-        assert_eq!(vd, vd2);
-        assert_eq!(vd.uuid(), vd2.uuid());
-        assert_eq!(vd.location(), Obj::mk_id(1));
-        assert_eq!(vd.owner(), Obj::mk_id(1));
-        assert_eq!(vd.names(), vec!["foo".to_string(), "bar".to_string()]);
-        assert_eq!(vd.flags(), VerbFlag::rwxd());
-        assert_eq!(
-            vd.binary_type(),
-            crate::model::verbs::BinaryType::LambdaMoo18X
-        );
-        assert_eq!(vd.args(), VerbArgsSpec::this_none_this(),);
-    }
-
-    #[test]
-    fn test_reconstitute_in_verbdefs() {
-        let vd1 = VerbDef::new(
-            uuid::Uuid::new_v4(),
-            Obj::mk_id(1),
-            Obj::mk_id(1),
-            &["foo", "bar"],
-            VerbFlag::rwxd(),
-            crate::model::verbs::BinaryType::None,
-            VerbArgsSpec::this_none_this(),
-        );
-
-        let vd2 = VerbDef::new(
-            uuid::Uuid::new_v4(),
-            Obj::mk_id(1),
-            Obj::mk_id(1),
-            &["zoinks", "zaps", "chocolates"],
-            VerbFlag::rx(),
-            crate::model::verbs::BinaryType::LambdaMoo18X,
-            VerbArgsSpec::this_none_this(),
-        );
-
-        let vd1_id = vd1.uuid();
-        let vd2_id = vd2.uuid();
-
-        let vds = VerbDefs::from_items(&[vd1, vd2]);
-        let bytes = vds.make_copy_as_vec().unwrap();
-        let vds2 = VerbDefs::from_bytes(ByteView::from(bytes)).unwrap();
-        let rvd1 = vds2.find(&vd1_id).unwrap();
-        let rvd2 = vds2.find(&vd2_id).unwrap();
-        assert_eq!(rvd1.uuid(), vd1_id);
-        assert_eq!(rvd1.location(), Obj::mk_id(1));
-        assert_eq!(rvd1.owner(), Obj::mk_id(1));
-        assert_eq!(rvd1.names(), vec!["foo".to_string(), "bar".to_string()]);
-        assert_eq!(rvd1.flags(), VerbFlag::rwxd(),);
-        assert_eq!(rvd1.binary_type(), crate::model::verbs::BinaryType::None);
-        assert_eq!(rvd1.args(), VerbArgsSpec::this_none_this(),);
-
-        assert_eq!(rvd2.uuid(), vd2_id);
-        assert_eq!(rvd2.location(), Obj::mk_id(1));
-        assert_eq!(rvd2.owner(), Obj::mk_id(1));
-        assert_eq!(
-            rvd2.names(),
-            vec![
-                "zoinks".to_string(),
-                "zaps".to_string(),
-                "chocolates".to_string()
-            ]
-        );
-        assert_eq!(rvd2.flags(), VerbFlag::rx());
-        assert_eq!(
-            rvd2.binary_type(),
-            crate::model::verbs::BinaryType::LambdaMoo18X
-        );
-        assert_eq!(rvd2.args(), VerbArgsSpec::this_none_this());
-    }
-
-    #[test]
-    fn test_empty_names() {
-        let vd1 = VerbDef::new(
-            uuid::Uuid::new_v4(),
-            Obj::mk_id(1),
-            Obj::mk_id(1),
-            &[],
-            VerbFlag::rwxd(),
-            crate::model::verbs::BinaryType::None,
-            VerbArgsSpec::this_none_this(),
-        );
-
-        let bytes = vd1.with_byte_buffer(<[u8]>::to_vec).unwrap();
-        let vd2 = VerbDef::from_bytes(ByteView::from(bytes));
-        assert_eq!(vd1, vd2);
-        assert_eq!(vd1.names(), Vec::<String>::new());
     }
 }
