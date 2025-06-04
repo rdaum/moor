4ec39f6811ad03d374bf9b293be09b914ace1093
Ryan Daum <ryan.daum@gmail.com>
2025-06-03 21:55:24 -0400

    Iteration on the pet datalog
      * Add incremental rule evaluation
      * Add bitmap index for optimized fun
      * Add benchmark to measure improvements

--- crates/common/benches/datalog_benches.rs
+++ crates/common/benches/datalog_benches.rs
@@ -1,3 +1,16 @@
+// Copyright (C) 2025 Ryan Daum <ryan.daum@gmail.com> This program is free
+// software: you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation, version
+// 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use moor_common::datalog::{Atom, Datalog, Rule, Term}; // Removed Fact import
 use moor_var::{Symbol, v_int, v_str, v_string};
