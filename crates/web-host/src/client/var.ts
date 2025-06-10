6041dcd3815a5f6b3c63c9a1f2b082268e9b3a64
Ryan Daum <ryan.daum@gmail.com>
2025-06-10 07:38:35 -0400

    Update var.ts

--- crates/web-host/src/client/var.ts
+++ crates/web-host/src/client/var.ts
@@ -11,22 +11,6 @@
 // this program. If not, see <https://www.gnu.org/licenses/>.
 //
 
-// Parse a JSON document representing a MOO 'Var'.
-// Moor JSON common are a bit special because we have a number of types that are not a direct map.
-
-// Represents a MOO 'map' which is a list of key-value pairs in sorted order and binary search for keys.
-// (We cannot use a JavaScript object because the keys are potentially-not strings.)
-// - Maps are not supported in JSON serialization, so we have to encode them as a list of pairs,
-//   with a tag to indicate that it's a map.
-// - Object references are encoded as a JSON object with a tag to indicate the type of reference.
-//      { oid: 1234 }
-// - Errors are encoded as a JSON object with a tag to indicate the type of error, and an optional description.
-//      { error: "E_PROPNF", message: "Property not found" }
-// - Lists are encoded as JSON arrays.
-// - Strings are encoded as JSON strings.
-// - Integers & floats are encoded as JSON numbers, but there's a caveat here that JSON's spec
-//   can't permit a full 64-bit integer, so we have to be careful about that.
-// - Future things like WAIFs, etc. will need to be encoded in a way that makes sense for JSON.
 
 export enum ORefKind {
     Oid,
@@ -51,6 +35,8 @@ export interface ObjMatch {
 
 export type ObjectRef = Oid | SysObj | ObjMatch;
 
+// Parse a JSON document representing a MOO 'Var'.
+// Moor JSON common are a bit special because we have a number of types that are not a direct map.
 export function jsonToValue(json: JSON) {
     if (typeof json === "number") {
         return json;
@@ -114,6 +100,19 @@ export class Error {
     }
 }
 
+// Represents a MOO 'map' which is a list of key-value pairs in sorted order and binary search for keys.
+// (We cannot use a JavaScript object because the keys are potentially-not strings.)
+// - Maps are not supported in JSON serialization, so we have to encode them as a list of pairs,
+//   with a tag to indicate that it's a map.
+// - Object references are encoded as a JSON object with a tag to indicate the type of reference.
+//      { oid: 1234 }
+// - Errors are encoded as a JSON object with a tag to indicate the type of error, and an optional description.
+//      { error: "E_PROPNF", message: "Property not found" }
+// - Lists are encoded as JSON arrays.
+// - Strings are encoded as JSON strings.
+// - Integers & floats are encoded as JSON numbers, but there's a caveat here that JSON's spec
+//   can't permit a full 64-bit integer, so we have to be careful about that.
+// - Future things like WAIFs, etc. will need to be encoded in a way that makes sense for JSON.
 export class Map {
     pairs: Array<[any, any]>;
 
