a3c2e3de0b7b84287fa3a38ba3715c9060a26c34
Ryan Daum <ryan.daum@gmail.com>
2025-08-28 15:43:19 -0400

    Fix ordering of properties in objdef dumps

--- crates/objdef/src/dump.rs
+++ crates/objdef/src/dump.rs
@@ -155,8 +155,8 @@ pub fn collect_object(
     }
 
     // Alphabetize properties. Verbs should remain in their original order.
-    od.property_definitions.sort_by(|a, b| a.name.cmp(&b.name));
-    od.property_overrides.sort_by(|a, b| a.name.cmp(&b.name));
+    od.property_definitions.sort_by(|a, b| a.name.as_arc_string().cmp(&b.name.as_arc_string()));
+    od.property_overrides.sort_by(|a, b| a.name.as_arc_string().cmp(&b.name.as_arc_string()));
     Ok((num_verbdefs, num_propdefs, num_propoverrides, od))
 }
 
