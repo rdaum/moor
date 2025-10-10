c22b346896a34f030bcc08a75d800fb4db0e1930
Ryan Daum <ryan.daum@gmail.com>
2025-10-09 21:59:42 -0400

    Attempt to add action for building debian packages

---
+++ deploy/build-web-client-deb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+# Build script for moor-web-client debian package
+# This script creates a debian package containing the built web client static files
+
+VERSION="0.9.0-alpha"
+PACKAGE_NAME="moor-web-client"
+BUILD_DIR="debian-pkg-web-client"
+
+echo "Building ${PACKAGE_NAME} ${VERSION}..."
+
+# Clean up any previous build
+rm -rf ${BUILD_DIR}
+rm -f ${PACKAGE_NAME}_${VERSION}_all.deb
+
+# Create package directory structure
+mkdir -p ${BUILD_DIR}/DEBIAN
+mkdir -p ${BUILD_DIR}/usr/share/moor/web-client
+mkdir -p ${BUILD_DIR}/usr/share/doc/${PACKAGE_NAME}
+
+# Copy built web client files
+echo "Copying web client files from dist/..."
+cp -r dist/* ${BUILD_DIR}/usr/share/moor/web-client/
+
+# Copy documentation
+cp deploy/nginx-example.conf ${BUILD_DIR}/usr/share/doc/${PACKAGE_NAME}/
+cp deploy/README.web-client.md ${BUILD_DIR}/usr/share/doc/${PACKAGE_NAME}/README.md
+
+# Create control file
+cat > ${BUILD_DIR}/DEBIAN/control <<EOF
+Package: ${PACKAGE_NAME}
+Version: ${VERSION}
+Architecture: all
+Maintainer: Ryan Daum <ryan.daum@gmail.com>
+Description: Web client for mooR
+ Modern web client for mooR - a 21st century LambdaMOO implementation.
+ This package contains the built static files that can be served by any
+ web server (nginx, caddy, apache, etc).
+Section: web
+Priority: optional
+Recommends: nginx
+EOF
+
+# Build the package
+echo "Building debian package..."
+dpkg-deb --build ${BUILD_DIR} ${PACKAGE_NAME}_${VERSION}_all.deb
+
+echo "Package built successfully: ${PACKAGE_NAME}_${VERSION}_all.deb"
+
+# Clean up
+rm -rf ${BUILD_DIR}
