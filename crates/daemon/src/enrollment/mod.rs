6e389ba5fd85ad521ad92e6fdfb5dbf5eb1fc0b9
Ryan Daum <ryan.daum@gmail.com>
2025-10-21 18:35:27 -0400

    Add missing license headers

--- crates/daemon/src/enrollment/mod.rs
+++ crates/daemon/src/enrollment/mod.rs
@@ -1,3 +1,16 @@
+// Copyright (C) 2025 Ryan Daum <ryan.daum@gmail.com> This program is free
+// software: you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation, version
+// 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
 //! Host enrollment server - accepts unauthenticated connections for initial CURVE key registration
 //!
 //! This module provides a separate ZMQ endpoint where hosts can enroll themselves by providing:
