ec9f4a26b4f6dbaab01af860068b5c5519d7a3c8
Ryan Daum <ryan.daum@gmail.com>
2025-11-18 13:30:07 -0500

    Fix 1.0.0 release build. Again.
     * Fix version number on web-client
     * Have one workflow which creates codeberg release, kicks off both
       the debian and docker builds and hopefully attaches the assets.

--- deploy/debian-packages/build-web-client-deb.sh
+++ deploy/debian-packages/build-web-client-deb.sh
@@ -17,7 +17,7 @@ set -e
 # Build script for moor-web-client debian package
 # This script creates a debian package containing the built web client static files
 
-VERSION="0.9.0-alpha-1"
+VERSION="1.0.0~beta1-1"
 PACKAGE_NAME="moor-web-client"
 BUILD_DIR="debian-pkg-web-client"
 
