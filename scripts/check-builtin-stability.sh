277bc49888eaf963745fc10c3e853e4d9f48d32a
Ryan Daum <ryan.daum@gmail.com>
2025-12-02 17:33:03 -0500

    Add missing license for shell script

--- scripts/check-builtin-stability.sh
+++ scripts/check-builtin-stability.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Copyright (C) 2025 Ryan Daum <ryan.daum@gmail.com> This program is free
+# software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, version
+# 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
 # Check that the builtin table in builtins.rs hasn't been modified in ways
 # that would invalidate compiled programs (removals, reorders, renames).
 # Adding new builtins at the end is allowed.
