44da0f2c0504a494f9cdd2685def96e19f5886fb
Ryan Daum <ryan.daum@gmail.com>
2026-01-01 08:47:12 -0500

    Fix remainder license headers

--- tools/moorc/src/main.rs
+++ tools/moorc/src/main.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Ryan Daum <ryan.daum@gmail.com> This program is free
+// Copyright (C) 2026 Ryan Daum <ryan.daum@gmail.com> This program is free
 // software: you can redistribute it and/or modify it under the terms of the GNU
 // General Public License as published by the Free Software Foundation, version
 // 3.
@@ -10,6 +10,8 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
 //
+
+
 #![recursion_limit = "256"]
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 mod feature_args;
