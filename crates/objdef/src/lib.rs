8891377337928d9b1ca96856d93a60208e7b3fda
Ryan Daum <ryan.daum@gmail.com>
2025-12-31 22:34:22 -0500

    License header fix

--- crates/objdef/src/lib.rs
+++ crates/objdef/src/lib.rs
@@ -10,6 +10,8 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
 //
+
+
 #![recursion_limit = "256"]
 
 mod dump;
