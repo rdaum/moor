a67de3f7cdc1a387a4d00cf3b1a1ef3f234eb23c
Ryan Daum <ryan.daum@gmail.com>
2026-02-08 13:24:05 -0500

    [fix] missing License headers

--- clients/moor-web-mcp/src/config.ts
+++ clients/moor-web-mcp/src/config.ts
@@ -9,6 +9,19 @@
 //
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+// Copyright (C) 2026 Ryan Daum <ryan.daum@gmail.com> This program is free
+// software: you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation, version
+// 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
 
 import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
